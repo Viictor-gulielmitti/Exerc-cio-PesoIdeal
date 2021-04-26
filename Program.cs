40618de9a4ec34da70f1d18c6021b7b4b4224b94
Viictor-gulielmitti <Victorgulielmitti284@gmail.com>
2021-04-26 19:45:14 -0300

    pesoideal

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exerc_cio_PesoIdeal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string  Sexo ;
+            duplo  numero , resposta ;
+
+            
+        Console.WriteLine ("Peso ideal");
+        Console.Write ("Digite sua altura em m ...: " );
+        
+        numero  =  Convert.ToDouble(Console.ReadLine());
+        Console.Write ("Sexo [m] asculino / [f] eminino ..." );
+         Sexo =  Console . ReadLine ();
+
+        if ( Sexo  ==  " m " ) {
+        resposta  = ( numero  *  72,7 ) - 58;
+        Console . WriteLine ($"Seu peso ideal é { resposta } kg " );
+
+        }
+        else  if ( Sexo  ==  " f " ) {
+        
+        resposta  = ( numero  *  62,1 ) -  44 ;
+        
+        Console . WriteLine ($" Seu peso ideal é { resposta } kg " );
+        
+    }
+
+    
+        }
+    }
+}
+
